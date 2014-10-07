82a6ce81417e621e2f3de67a4d86a0a6f91e0d0d
concreted <arichuang@gmail.com>
2014-10-07 13:38:30 -0700

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 brtrkr
 ======
+
+
+API Key
+-------
+
+This is publically available BART API Key being used. 
+
+MW9S-E7SL-26DU-VV8V
---
+++ app.js
@@ -0,0 +1,18 @@
+var Bart = require('bart-api');
+
+var key = 'MW9S-E7SL-26DU-VV8V';
+
+//var bart = new Bart(key);
+
+// bart.sched.arrive({
+//   orig: 'ucty',
+//   dest: 'powl'
+// }, function (err, xml, body) {
+//   if (err) {
+//     throw err;
+//   }
+
+//   console.log(body);
+
+// })
+;
